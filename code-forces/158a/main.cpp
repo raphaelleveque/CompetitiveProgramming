8a5209e515d7b0f4c0ed996343a3f1f7c724cce2
Raphael Leveque <raphaelleveque@Raphaels-MacBook-Air.local>
2021-10-07 17:36:43 -0300

    158A- Next Round

---
+++ code-forces/158a/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> scores(n);
+    for (int i = 0; i < n; i++)
+        cin >> scores[i];
+    int min = scores[k-1];
+    int counter = 0;
+    for (int i = 0; i < n; ++i) {
+        if (scores[i] >= min && scores[i] > 0)
+            counter++;
+    }
+    cout << counter;
+}
