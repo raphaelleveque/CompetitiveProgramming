44cc4f9ad2f9fb5aaef6adb1b77f4607c36910a8
Raphael Leveque <raphaelleveque@Raphaels-MacBook-Air.local>
2021-10-07 17:55:06 -0300

    50A

---
+++ code-forces/50a/cmake-build-debug/CMakeFiles/50a.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "50a"
+  "50a.pdb"
+  "CMakeFiles/50a.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/50a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
