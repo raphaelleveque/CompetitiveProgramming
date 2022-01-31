8c5acee75294e1dd017e53c3b017e6833bdc3442
Raphael Leveque <raphaellev1102@gmail.com>
2022-01-31 20:49:42 -0300

    1365 v2 leetcode

--- leetcode/1365/v2.cpp
+++ leetcode/1365/v2.cpp
@@ -11,11 +11,15 @@ public:
         
         // Put the values in hash-map according to their values corresponding to their places in nums 
         // T.C = O(n)
+	// The forloop is going backwards, so if there are any duplicate values, the map will only consider the smallest 'i'
+	// That will help us because the map will not count the current number as smaller than itself
         for(int i=n-1; i>=0; i--) {
+	    // Now, because the array is sorted, we can say that 'i' is the number of elements smaller
+            // than the current number (copy[i])
             m[vec[i]] = i;
         }
         
-        // Saving the result again in nums, b'cos space complexity matters XD
+        // Saving the result again in nums, because space complexity matters XD
         // T.C = O(n)
         for(int i=0; i<n; i++) {
             nums[i] = m[nums[i]];
