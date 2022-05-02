e640d175f6b5e85df5724ea6022925a33fa82c0e
Raphael Leveque <raphaellev1102@gmail.com>
2022-05-02 15:46:22 -0300

    leetcode 1282 v1

--- leetcode/1282/v1.cpp
+++ leetcode/1282/v1.cpp
@@ -3,12 +3,17 @@ public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
         vector<vector<int>> ans;
         
+        // First, I paired up the ID with the index of each number
         vector<pair<int, int>> groupNindex;
         for (int i = 0; i < groupSizes.size(); i++)
             groupNindex.push_back(make_pair (groupSizes[i], i));
         
+        // Then, I sorted the ID's
         sort(groupNindex.begin(), groupNindex.end());
 
+        // Now that it's sorted, I created a temporary vector that will be pushed back to the matrix
+        // Because it's sorted, I know there will be at least 3 numbers 3 in sequence 
+        // to push back its index to the temporary array
         vector<pair<int, int>>::iterator it = groupNindex.begin();
         while (it != groupNindex.end()) {
             vector<int> tmp;
@@ -17,7 +22,6 @@ public:
                 tmp.push_back(it->second);
             
             ans.push_back(tmp);
-            
         }
         return ans;
     }
