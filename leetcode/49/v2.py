1400b2072877a74d7e0fa5e2328e70c9ddb6575c
Raphael Leveque <raphaellev1102@gmail.com>
2024-06-14 09:25:39 -0300

    49

--- leetcode/49/v2.py
+++ leetcode/49/v2.py
@@ -3,10 +3,16 @@ class Solution:
         mymap = {}
 
         for s in strs:
-            hashmap_frequencia = frozenset(Counter(s).items())
-            mymap.setdefault(hashmap_frequencia, []).append(s)
-        
-        ans = []
-        for s in mymap.values():
-            ans.append(s)
-        return ans
+            # Criando um map com os caracteres da string
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+
+            # Cada string que possuir essa mesma combinação de 
+            # caracteres irá ser mapeada
+            str_map = tuple(count)
+            if str_map in mymap:
+                mymap[str_map].append(s)
+            else:
+                mymap[str_map] = [s]
+        return list(mymap.values())
